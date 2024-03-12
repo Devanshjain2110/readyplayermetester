6170214018dcf70e8bd1c375677cace4a3070b91
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-03-12 19:50:46 +0530

    Added avatarr rendering feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
+import { Avatar } from '@readyplayerme/visage';
+import { useState } from 'react';
 
 const config: AvatarCreatorConfig = {
-  clearCache: true,
-  bodyType: 'fullbody',
-  quickStart: true,
+  clearCache: false,
+  bodyType: 'halfbody',
+  quickStart: false,
   language: 'en',
 };
 
-const style = { width: '1500px', height: '100vh', border: 'none' };
+const style = { width: '1100px', height: '100vh', border: 'none' };
 
 export default function App() {
+  const [avatarUrl, setAvatarUrl] = useState('');
   const handleOnAvatarExported = (event: AvatarExportedEvent) => {
-    console.log(`Avatar URL is: ${event.data.url}`);
+    console.log(event.data.url)
+    setAvatarUrl(event.data.url);
+
   };
 
   return (
     <div className=''>
-      <AvatarCreator subdomain="https://readyplayermetest-0xe5du.readyplayer.me?frameApi" config={config} style={style} onAvatarExported={handleOnAvatarExported} />
+      <AvatarCreator  subdomain="localhost-kgqvbg" config={config} style={style} onAvatarExported={handleOnAvatarExported}  />
+      {avatarUrl && <Avatar modelSrc={avatarUrl} />}
     </div>
   );
 }
